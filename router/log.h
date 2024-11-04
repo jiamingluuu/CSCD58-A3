1ffff333fc64ac78675c02dc58bffe2f352ca0a8
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-03 23:35:38 -0500

    Update log.h

--- router/log.h
+++ router/log.h
@@ -14,13 +14,13 @@
 #define LOG_DEBUG(format, ...)
 #endif
 
-#define LOG_INFO(format, ...) \
-  fprintf(stdout, "INFO [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
+#define LOG_INFO(format, ...) printf("DEBUG [%s:%d] " format "\n");
+// fprintf(stdout, "INFO [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
 
-#define LOG_WARN(format, ...) \
-  fprintf(stdout, "WARN [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
+#define LOG_WARN(format, ...) printf("DEBUG [%s:%d] " format "\n");
+// fprintf(stdout, "WARN [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
 
-#define LOG_ERROR(format, ...) \
-  fprintf(stdout, "ERROR [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
+#define LOG_ERROR(format, ...) printf("DEBUG [%s:%d] " format "\n");
+// fprintf(stdout, "ERROR [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
 
 #endif /* __LOG_h */
