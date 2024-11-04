d0f6152a2d128c9b602f0b364d6fb4eb312da741
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-03 23:31:05 -0500

    Update log.h

--- router/log.h
+++ router/log.h
@@ -3,36 +3,30 @@
 
 #define DEBUG
 
-/* #include <cstring> */ 
-#include <string.h>
+/* #include <cstring> */
 #include <stdio.h>
+#include <string.h>
 
 #ifdef DEBUG
-#define LOG_DEBUG(format, ...)                                                 \
-  {                                                                            \
-    fprintf(stdout, "DEBUG [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1,  \
-            __LINE__, ##__VA_ARGS__);                                          \
-  }
+#define LOG_DEBUG(format, ...) \
+  { fprintf(stdout, "DEBUG [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__); }
 #else
 #define LOG_DEBUG(format, ...)
 #endif
 
-#define LOG_INFO(format, ...)                                                  \
-  do {                                                                         \
-    fprintf(stdout, "INFO [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1,   \
-            __LINE__, ##__VA_ARGS__);                                          \
+#define LOG_INFO(format, ...)                                                                          \
+  do {                                                                                                 \
+    fprintf(stdout, "INFO [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__); \
   } while (0)
 
-#define LOG_WARN(format, ...)                                                  \
-  do {                                                                         \
-    fprintf(stdout, "WARN [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1,   \
-            __LINE__, ##__VA_ARGS__);                                          \
+#define LOG_WARN(format, ...)                                                                          \
+  do {                                                                                                 \
+    fprintf(stdout, "WARN [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__); \
   } while (0)
 
-#define LOG_ERROR(format, ...)                                                 \
-  do {                                                                         \
-    fprintf(stdout, "ERROR [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1,  \
-            __LINE__, ##__VA_ARGS__);                                          \
+#define LOG_ERROR(format, ...)                                                                          \
+  do {                                                                                                  \
+    fprintf(stdout, "ERROR [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__); \
   } while (0)
 
-#endif /* __LOG_h */ 
+#endif /* __LOG_h */
