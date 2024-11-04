e3df84870cb98f5df9234bc72a2e4b3499649f2d
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-03 23:38:02 -0500

    Update log.h

--- router/log.h
+++ router/log.h
@@ -7,15 +7,15 @@
 #include <string.h>
 
 #ifdef DEBUG
-#define LOG_DEBUG(format, ...) printf("DEBUG [%s:-] " format "\n");
+#define LOG_DEBUG(format, ...) printf("DEBUG [-:-] " format "\n");
 #else
 #define LOG_DEBUG(format, ...)
 #endif
 
-#define LOG_INFO(format, ...) printf("DEBUG [%s:-] " format "\n");
+#define LOG_INFO(format, ...) printf("DEBUG [-:-] " format "\n");
 
-#define LOG_WARN(format, ...) printf("DEBUG [%s:-] " format "\n");
+#define LOG_WARN(format, ...) printf("DEBUG [-:-] " format "\n");
 
-#define LOG_ERROR(format, ...) printf("DEBUG [%s:-] " format "\n");
+#define LOG_ERROR(format, ...) printf("DEBUG [-:-] " format "\n");
 
 #endif /* __LOG_h */
