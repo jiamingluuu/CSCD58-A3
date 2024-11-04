597789e7bb7d837ebc8e7a6c968e6e55f2b3232d
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-03 23:36:14 -0500

    Update log.h

--- router/log.h
+++ router/log.h
@@ -3,24 +3,19 @@
 
 #define DEBUG
 
-/* #include <cstring> */
 #include <stdio.h>
 #include <string.h>
 
 #ifdef DEBUG
 #define LOG_DEBUG(format, ...) printf("DEBUG [%s:%d] " format "\n");
-// fprintf(stdout, "DEBUG [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(format, ...)
 #endif
 
 #define LOG_INFO(format, ...) printf("DEBUG [%s:%d] " format "\n");
-// fprintf(stdout, "INFO [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
 
 #define LOG_WARN(format, ...) printf("DEBUG [%s:%d] " format "\n");
-// fprintf(stdout, "WARN [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
 
 #define LOG_ERROR(format, ...) printf("DEBUG [%s:%d] " format "\n");
-// fprintf(stdout, "ERROR [%s:%d] " format "\n", strrchr(__FILE__, '/') + 1, __LINE__, ##__VA_ARGS__);
 
 #endif /* __LOG_h */
