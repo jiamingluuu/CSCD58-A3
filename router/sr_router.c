5f626fd20b20a47931f385abca6f6db0de2b8035
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-05 01:21:21 -0500

    Update sr_router.c

--- router/sr_router.c
+++ router/sr_router.c
@@ -427,7 +427,7 @@ static void send_icmp_response(struct sr_instance *sr, uint8_t *packet, unsigned
   printf("response_ip_hdr: %p\n", response_ip_hdr);
   printf("request_ip_hdr: %p\n", request_ip_hdr);
   printf("sizeof(sr_ip_hdr_t): %zu\n", sizeof(sr_ip_hdr_t));
-  printf("%d %d",response_len,sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t)
+  printf("%d %d", response_len, sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
   printf("%d", ((uint8_t *)response_ip_hdr)[0]);
   printf("%d", ((uint8_t *)request_ip_hdr)[0]);
   memcpy(response_ip_hdr, request_ip_hdr, sizeof(sr_ip_hdr_t));
